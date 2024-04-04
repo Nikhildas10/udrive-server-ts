65e40513e99ebe19f498092ca43225adbe990868
Nikhildas10 <nikhildas1027@gmail.com>
2024-04-04 10:40:37 +0530

    updated

--- app.ts
+++ app.ts
@@ -15,7 +15,7 @@ app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["https://u-drive-three.vercel.app"],
+    origin: ["*"],
     credentials: true,
   })
 );
