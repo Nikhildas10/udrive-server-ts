4ff6fb50e05cd9d78e4a64fc6f82f443bb8abb65
Nikhildas10 <nikhildas1027@gmail.com>
2024-04-05 18:11:38 +0530

    updated

--- routes/car.route.ts
+++ routes/car.route.ts
@@ -8,7 +8,7 @@ import { addCars, deleteCar, editCar, getAllCar, getSingleCar } from "../control
 const carRouter = express.Router();
 
 carRouter.post("/create",isAuthenticated,addCars);
-carRouter.put("/edit/:id", isAuthenticated,multipleUpload.array("files"), editCar);
+carRouter.put("/edit/:id", isAuthenticated, editCar);
 carRouter.delete("/delete/:id", isAuthenticated, deleteCar);
 carRouter.get("/get-all-cars", isAuthenticated, getAllCar);
 carRouter.get("/get/:id", isAuthenticated, getSingleCar);
