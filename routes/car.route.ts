84556d1aa209c9a4ec6048fc87ca2988acdbf63a
Nikhildas10 <nikhildas1027@gmail.com>
2024-05-04 16:15:40 +0530

    fixed running cars and cars on yard

--- routes/car.route.ts
+++ routes/car.route.ts
@@ -4,6 +4,7 @@ import { isAuthenticated } from "../middleware/auth";
 import upload from "../middleware/multer";
 import multipleUpload from "../middleware/multer";
 import { addCars, carsOnYard, deleteCar, deleteMultipleCars, editCar, getAllCar, getBookedCars, getCarTotalRevenue, getMostBookedCars, getNonBookedCars, getSingleCar, runningCars } from "../controllers/car.controller";
+import { getDashboardData } from "../controllers/dashboard.controller";
 
 const carRouter = express.Router();
 
@@ -11,7 +12,8 @@ carRouter.post("/create",isAuthenticated,addCars);
 carRouter.put("/edit/:id", isAuthenticated, editCar);
 carRouter.delete("/delete/:id", isAuthenticated, deleteCar);
 carRouter.get("/get-all-cars", isAuthenticated, getAllCar);
-carRouter.get("/most-booked-cars", isAuthenticated, getMostBookedCars);
+// carRouter.get("/most-booked-cars", isAuthenticated, getMostBookedCars);
+carRouter.get("/most-booked-cars", isAuthenticated, getDashboardData);
 carRouter.delete("/multiple-delete", isAuthenticated, deleteMultipleCars);
 carRouter.get("/get-running-cars", isAuthenticated, runningCars);
 carRouter.get("/get-cars-on-yard", isAuthenticated, carsOnYard);
