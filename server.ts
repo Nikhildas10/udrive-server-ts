d839ac00b3fb1ad03e482f900878bc9bfc937935
Nikhildas10 <nikhildas1027@gmail.com>
2024-05-17 10:09:08 +0530

    added socket io event

--- server.ts
+++ server.ts
@@ -3,8 +3,6 @@ const { Server } = require("socket.io");
 import { app } from "./app";
 import { connectDb } from "./utils/db";
 import { v2 as cloudinary } from "cloudinary";
-const http = require("http");
-
 //cloudinary config
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -12,13 +10,15 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_SECRET_KEY,
   timeout: 60000,
 });
-const PORT = process.env.PORT || 3000;
-const server = http.createServer(app);
+app.listen(process.env.PORT, () => {
+  console.log(`connected to port ${process.env.PORT}`);
+  connectDb();
+});
 
-const io = new Server(server, {
+const io = require("socket.io")(5000, {
   cors: {
     origin: [
-      "https://u-drive-three.vercel.app", // Your deployed frontend URL
+      "https://u-drive-three.vercel.app",
       "http://localhost:3031",
       "http://localhost:3030",
     ],
@@ -27,17 +27,12 @@ const io = new Server(server, {
 });
 
 io.on("connection", (socket) => {
-  console.log("Socket connected");
-
+  console.log("socket connnected");
+  
   socket.on("disconnect", () => {
-    console.log("Socket disconnected");
+    console.log("socket disconnected");
+    
   });
-});
-
-// Start the server
-server.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-  connectDb();
 });
 export const emitSocketEvent=(event,payload)=>{
   io.emit(event,payload)
