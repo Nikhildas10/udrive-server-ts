0aab9dcd5b5bd6f55a75cad69575882c392a7987
Adhars raj <3301ad@gmail.com>
2024-11-20 09:04:47 +0530

    changes in cors

--- app.ts
+++ app.ts
@@ -32,6 +32,7 @@ app.use(
       "https://u-drive-three.vercel.app",
       "http://localhost:3031",
       "http://localhost:3030",
+      "https://u-drrive.vercel.app",
     ],
     credentials: true,
   })
